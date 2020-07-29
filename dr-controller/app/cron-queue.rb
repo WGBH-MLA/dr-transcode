5420450c4c1b2eb70c55ea6abbff2121b952dff3
foglabs <foglaboratories@gmail.com>
2020-07-29 09:54:56 -0400

    'rename'

---
+++ dr-controller/app/cron-queue.rb
@@ -0,0 +1,14 @@
+require 'aws-sdk-sqs'
+
+
+# load queue...
+# queue = Sqs.whatever
+
+# load db..
+# client = Mysql2::Client.new(host: "mysql", username: "root", database: "drtranscode", port: 3306)
+
+
+
+
+
+# CREATE TABLE status (status INTEGER, input_filename VARCHAR(1024);
