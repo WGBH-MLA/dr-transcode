9948509e1e6c7d070f6848a655b117b1dc5d5e84
foglabs <foglaboratories@gmail.com>
2020-08-05 10:10:19 -0400

    '68'

--- dr-controller/app/cron-queue.rb
+++ dr-controller/app/cron-queue.rb
@@ -82,7 +82,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg:66
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg:67
       volumeMounts:
       - mountPath: /root/.aws
         name: obstoresecrets
