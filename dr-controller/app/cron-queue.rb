6d703094498c7ed7291c55bcd75a8fec8f5ff4c0
foglabs <foglaboratories@gmail.com>
2020-08-20 11:50:51 -0400

    'version bump'

--- dr-controller/app/cron-queue.rb
+++ dr-controller/app/cron-queue.rb
@@ -122,7 +122,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg:86
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg:87
       volumeMounts:
       - mountPath: /root/.aws
         name: obstoresecrets
