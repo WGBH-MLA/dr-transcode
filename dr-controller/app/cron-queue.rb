507f82a0dcaef74b5c9413391d6b43669207c573
foglabs <foglaboratories@gmail.com>
2021-05-14 14:43:59 -0400

    '140 wow'

--- dr-controller/app/cron-queue.rb
+++ dr-controller/app/cron-queue.rb
@@ -189,7 +189,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg:139
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg:149
       resources:
         limits:
           memory: "5000Mi"
@@ -245,7 +245,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg-audiosplit:139
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg-audiosplit:140
       resources:
         limits:
           memory: "5000Mi"
