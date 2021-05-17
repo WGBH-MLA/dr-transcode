b299ac143f2fa36ca1835aa7a844b1e612015dc5
foglabs <foglaboratories@gmail.com>
2021-05-17 11:36:30 -0400

    '145'

--- dr-controller/app/cron-queue.rb
+++ dr-controller/app/cron-queue.rb
@@ -189,7 +189,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg:144
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg:145
       resources:
         limits:
           memory: "5000Mi"
@@ -245,7 +245,7 @@ spec:
         secretName: obstoresecrets
   containers:
     - name: dr-ffmpeg
-      image: mla-dockerhub.wgbh.org/dr-ffmpeg-audiosplit:144
+      image: mla-dockerhub.wgbh.org/dr-ffmpeg-audiosplit:145
       resources:
         limits:
           memory: "5000Mi"
